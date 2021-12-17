f92c41b0d970585abbdcff00b6e6a98977444063
佐藤航平 <sk1996kytk0409@i.softbank.jp>
2021-12-17 11:01:27 +0900

    dev 3.12.17 modal

--- public/js/top.js
+++ public/js/top.js
@@ -1,3 +1,12 @@
 {
-    'use strict'
+    'use strict';
+    const modal = document.getElementById('modal');
+    const menu = document.getElementById('menu');
+    const close = document.getElementById('close');
+    menu.addEventListener('click',() =>{
+        modal.classList.add('hidden');
+    })
+    close.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+    })
 }
