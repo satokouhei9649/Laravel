944579de47c1a366b131a5413111252783068070
佐藤航平 <sk1996kytk0409@i.softbank.jp>
2021-12-22 14:18:25 +0900

    dev 3 .12.22 cur

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -14,77 +14,83 @@
                 </form>
             </div>
 
+        <section>
+            <h2>オススメ商品 ・ セール</h2>
+            <ul>
+                <li class="screen sp">オススメ</li>
+                <li class="screen">セールス</li>
+                <li class="screen">オスメめ</li>
+                <li class="screen">セール</li>
+            </ul>
+            <button id="prev">&laquo;</button>
+            <button id="next">&raquo;</button>
+        </section>
 
 
+        <ul class="foodmenu">
+            <li><a href="#" class="foodIcon tab1" data-id="meat"><img src="{{ asset('img/steak.png')}}" ></a></li>
+            <li><a href="#" class="foodIcon tab2" data-id="fish"><i class="fas fa-fish fa-4x"></i></a></li>
+            <li><a href="#" class="foodIcon tab3" data-id="veg"><i class="fas fa-carrot fa-4x"></i></a></li>
+            <li><a href="#" class="foodIcon tab4" data-id="egg"><i class="fas fa-egg fa-4x"></i></a></li>
+            <li><a href="#" class="foodIcon tab5" data-id="fruit"><i class="fas fa-apple-alt fa-4x"></i></a></li>
+            <li><a href="#" class="foodIcon tab6" data-id="drink"><i class="fas fa-wine-bottle fa-4x"></i></a></li>
+            <li><a href="#" class="foodIcon tab7" data-id="others"><i class="fas fa-bread-slice fa-4x"></i></a></li>
+        </ul>
 
+        <section class="content active" id="meat" style="border: 20px solid rgb(238, 53, 53)">
+            <h1>肉</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section class="content" id="fish" style="border: 20px solid rgb(34, 86, 230)">
+            <h1>魚</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section class="content" id="veg" style="border: 20px solid rgb(14, 204, 14)">
+            <h1>野菜</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section class="content" id="egg" style="border: 20px solid rgb(250, 233, 3)">
+            <h1> 卵</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section class="content" id='fruit' style="border: 20px solid orange">
+            <h1>果物</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section class="content" id="drink" style="border: 20px solid pink">
+            <h1>飲み物</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+        <section class="content" id="others" style="border: 20px solid rgb(185, 26, 185)">
+            <h1>その他</h1>
+            <ul>
+                <li></li>
+            </ul>
+        </section>
 
-
-
-
-
-
-
-            
-
-                <ul class="foodmenu">
-                    <li><a href="#" class="foodIcon tab1" data-id="meat"><img src="{{ asset('img/steak.png')}}" ></a></li>
-                    <li><a href="#" class="foodIcon tab2" data-id="fish"><i class="fas fa-fish fa-4x"></i></a></li>
-                    <li><a href="#" class="foodIcon tab3" data-id="veg"><i class="fas fa-carrot fa-4x"></i></a></li>
-                    <li><a href="#" class="foodIcon tab4" data-id="egg"><i class="fas fa-egg fa-4x"></i></a></li>
-                    <li><a href="#" class="foodIcon tab5" data-id="fruit"><i class="fas fa-apple-alt fa-4x"></i></a></li>
-                    <li><a href="#" class="foodIcon tab6" data-id="drink"><i class="fas fa-wine-bottle fa-4x"></i></a></li>
-                    <li><a href="#" class="foodIcon tab7" data-id="others"><i class="fas fa-bread-slice fa-4x"></i></a></li>
-                </ul>
-
-                <section class="content active" id="meat" style="border: 20px solid rgb(238, 53, 53)">
-                    <h1>肉</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section class="content" id="fish" style="border: 20px solid rgb(34, 86, 230)">
-                    <h1>魚</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section class="content" id="veg" style="border: 20px solid rgb(14, 204, 14)">
-                    <h1>野菜</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section class="content" id="egg" style="border: 20px solid rgb(250, 233, 3)">
-                   <h1> 卵</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section class="content" id='fruit' style="border: 20px solid orange">
-                   <h1>果物</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section class="content" id="drink" style="border: 20px solid pink">
-                    <h1>飲み物</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-                <section class="content" id="others" style="border: 20px solid rgb(185, 26, 185)">
-                    <h1>その他</h1>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </section>
-
-
-
-
-
-
+        <section>
+            <h2>新着順</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
         </main>
+
         <footer>
             <p>お問合わせ</p>
             <p>新規登録 / ログイン</p>
